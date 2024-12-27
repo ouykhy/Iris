a2a44a287069d03747fa18f6a139b47e6b4226fd
ouykhy <ouykhy@gmail.com>
2024-12-27 11:55:49 +0100

    adding app

---
+++ app/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# In[3]:
+
+
+from fastapi import FastAPI, Body
+import joblib
+import numpy as np
+
+
+# In[14]:
+
+
+app = FastAPI()
+
+# Load the model when the application starts
+model = joblib.load("C:/Users/oquach/Documents/OpenClassRoom/Iris/models/trained_model.pkl")
+
+
+# In[15]:
+
+
+@app.get("/")
+def root():
+    return {"message": "API is running"}
+
+@app.post("/predict")
+@app.post("/predict")
+def predict(data: list = Body(...)):
+    arr = np.array(data)
+    preds = model.predict(arr).tolist()
+    return {"predictions": preds}
+
+
+# In[ ]:
+
+
+
+
---
+++ tests/test_api.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+import pytest
+from fastapi.testclient import TestClient
+import sys
+sys.path.append('C:/Users/oquach/Documents/OpenClassRoom/Iris/app')
+from main import app
+
+
+# In[27]:
+
+
+client = TestClient(app)
+
+
+# In[30]:
+
+
+def test_root():
+    res = client.get("/")
+    assert res.status_code == 200
+    assert "API is running" in res.json()["message"]
+
+def test_predict():
+    #payload = {"data": [[5.1, 3.5, 1.4, 0.2]]}
+    payload = [[5.1, 3.5, 1.4, 0.2]]
+    res = client.post("/predict", json=payload)
+    assert res.status_code == 200
+    assert "predictions" in res.json()
+
